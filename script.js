f67043d06d6d1c89a687a33780e44adbe9bf2347
Steven Zeng <zengstevenz@gmail.com>
2020-10-11 13:32:07 -0400

    Final Commit

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + ", Min: " + min + ", Seconds: " + sec);
+function runTheClock(){
+    sec+=6;
+    min+=1/10
+    hr+=1/120
+    HOURHAND.style.transform = "rotate(" + hr +  "deg)";
+    MINUTEHAND.style.transform = "rotate(" + min + "deg)";
+    SECONDHAND.style.transform = "rotate(" + sec + "deg)";
+}
+var interval = setInterval(runTheClock, 1000);
